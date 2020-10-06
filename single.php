fea956d6b0f3dd83b96dcdcb9bfb03872fb5e873
buistineke <buist.ineke@gmail.com>
2020-10-06 22:43:17 +0200

    Styles comment section and post navigation

--- single.php
+++ single.php
@@ -20,8 +20,22 @@ get_header();
 
 			the_post_navigation(
 				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'herbs' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'herbs' ) . '</span> <span class="nav-title">%title</span>',
+					'prev_text' => '<svg width="24px" height="10px" viewBox="0 0 24 10" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+					    <title>arrow-left</title>
+					    <g id="arrow-left" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+					        <g id="arrow" transform="translate(12.000000, 5.000000) scale(-1, 1) translate(-12.000000, -5.000000) " fill="#000000" fill-rule="nonzero">
+					            <polygon id="Path" points="22.64 4.485 22.64 5.735 0 5.735 0 4.485"></polygon>
+					            <polygon id="Path" points="18.5 10 17.65 9.10958904 22.16 5 17.65 0.900195695 18.5 0 24 5"></polygon>
+					        </g>
+					    </g>
+					</svg><span class="nav-subtitle">' . esc_html__( 'Previous:', 'herbs' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<svg width="24px" height="10px" viewBox="0 0 24 10" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+					    <title>arrow-right</title>
+					    <g id="arrow-right" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+					        <polygon id="Path" fill="#000000" fill-rule="nonzero" points="22.64 4.485 22.64 5.735 0 5.735 0 4.485"></polygon>
+					        <polygon id="Path" fill="#000000" fill-rule="nonzero" points="18.5 10 17.65 9.10958904 22.16 5 17.65 0.900195695 18.5 0 24 5"></polygon>
+					    </g>
+					</svg><span class="nav-subtitle">' . esc_html__( 'Next:', 'herbs' ) . '</span> <span class="nav-title">%title</span>',
 				)
 			);
 
